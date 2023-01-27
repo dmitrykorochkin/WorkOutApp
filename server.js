3b1849efb5d1115d9f9b37c8a61788d2397c3b41
Dmitry Korochkin <abramslost@gmail.com>
2023-01-27 14:26:58 +0300

    add morgan

---
+++ server.js
@@ -0,0 +1,22 @@
+import  express  from "express";
+import authRoutes from "./app/auth/auth.routes.js";
+
+const app = express();
+
+async function main() {
+   if(process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+  app.use(express.json());
+  app.use('api/auth/', authRoutes);
+
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(
+
+    PORT,
+    console.log(`🌈Server running is ${process.env.NODE_ENV} mode on port ${PORT}`)
+  );
+
+} 
+
+main();
