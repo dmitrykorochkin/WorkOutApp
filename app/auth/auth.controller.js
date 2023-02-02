6731968f8d25568e1a351b67fbe74b0808dd3a7d
Dmitry Korochkin <abramslost@gmail.com>
2023-02-02 11:40:26 +0300

    add file error.middleware

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,7 +1,12 @@
+import asyncHandler from 'express-async-handler';
 import { prisma } from "../prisma.js";
 
 
-export const authUser = async (req, res) => {
-  const user =  await prisma.user.findMany();
+export const authUser = asyncHandler ( async (req, res) => {
+  const user =  await prisma.user.findMany({
+    where: {
+      password1: '1123'
+    }
+  });
   res.json(user);
-};
+});
