fb5c6ac979121ed3266aa6abda8b20d9b2a4107f
Dmitry Korochkin <abramslost@gmail.com>
2023-02-08 16:14:36 +0300

    add updateExercise

--- app/exercise/exercise.controller.js
+++ app/exercise/exercise.controller.js
@@ -13,6 +13,24 @@ export const createNewExercise = asyncHandler(async (req, res) => {
   res.json(exercise);
 });
 
+export const updateExercise = asyncHandler(async (req, res) => {
+  const {name,times,iconPath} = req.body;
+
+try{
+    const exercise = await prisma.exercise.update({
+    where: {
+      id: +req.params.id
+    },
+    data: {
+      name, times, iconPath
+    }
+  });
+  res.json(exercise);
+} catch (error) {console.log(error);}
+
+  
+});
+
 export const getExercise = asyncHandler(async (req, res) => {
   const exercise = await prisma.exercise.findMany()
 
